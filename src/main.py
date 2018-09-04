a043de8f5e2910ec74ea219b2bb9994c05c4564e
Orix <orix.auyeung@hunt.social>
2018-09-04 13:33:55 +0800

    add port config and start.sh

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from twisted.internet import reactor
 from twisted.web.server import Site
 from twisted.web.wsgi import WSGIResource
 import werkzeug.serving
+import argparse
 
 app = Flask(__name__)
 handler = JSONExceptionHandler(app)
@@ -16,11 +17,17 @@ app.register_blueprint(modelController)
 def status():
     return response("Status OK, ready to receive data.")
 
-def run_twisted_wsgi():
+def run_twisted_wsgi(port=8000):
     resource = WSGIResource(reactor, reactor.getThreadPool(), app)
     site = Site(resource)
-    reactor.listenTCP(5000, site)
+    reactor.listenTCP(port, site)
     reactor.run(**reactor_args)
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='GroupAPI')
+    parser.add_argument('-p', '--port', help='Port to listen on', required=False)
+    args = parser.parse_args()
+    return args
 
 if __name__ == "__main__":
     # app.run()     # DO NOT USE FOR PRODUCTION
@@ -28,5 +35,9 @@ if __name__ == "__main__":
     if app.debug:
         reactor_args['installSignalHandlers'] = 0       # Disable twisted signal handlers in development only.
         run_twisted_wsgi = werkzeug.serving.run_with_reloader(run_twisted_wsgi)     # Turn on auto reload.
-        
-    run_twisted_wsgi()
+    args = parse_arguments()
+
+    if not args.port is None and not isinstance(args.port, list) and args.port.isdigit():
+        run_twisted_wsgi(int(args.port))
+    else:
+        run_twisted_wsgi()
---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+port=8000
+scriptDir=$(dirname "$0")
+cd $scriptDir
+nohup ./venv/bin/python3 ./src/main.py -p $port &
