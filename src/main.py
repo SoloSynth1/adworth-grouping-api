87a4504f7011d628c8dd7f0854ddf622494614ba
Orix <orix.auyeung@hunt.social>
2018-09-04 13:04:30 +0800

    add twisted WSGI server`

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ from flask import Flask
 from controller.ExceptionHandler import JSONExceptionHandler
 from controller.ModelController import modelController
 from controller.Responses import response
+from twisted.internet import reactor
+from twisted.web.server import Site
+from twisted.web.wsgi import WSGIResource
+import werkzeug.serving
 
-port = 5000
 app = Flask(__name__)
 handler = JSONExceptionHandler(app)
 app.register_blueprint(modelController)
@@ -13,6 +16,17 @@ app.register_blueprint(modelController)
 def status():
     return response("Status OK, ready to receive data.")
 
+def run_twisted_wsgi():
+    resource = WSGIResource(reactor, reactor.getThreadPool(), app)
+    site = Site(resource)
+    reactor.listenTCP(5000, site)
+    reactor.run(**reactor_args)
 
 if __name__ == "__main__":
-    app.run(port=port)
+    # app.run()     # DO NOT USE FOR PRODUCTION
+    reactor_args = {}
+    if app.debug:
+        reactor_args['installSignalHandlers'] = 0       # Disable twisted signal handlers in development only.
+        run_twisted_wsgi = werkzeug.serving.run_with_reloader(run_twisted_wsgi)     # Turn on auto reload.
+        
+    run_twisted_wsgi()
